fe02ba7927f6e3fabf95cfe3a825360764a6327d
Sw720259 <swathi.v1@s.amity.edu>
2025-03-18 21:51:44 +0530

    print the grade using switch statement

---
+++ switc.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main() {
+	int grade;
+	printf("enter a number:%d\n");
+	scanf("%d",&grade);
+
+	switch(grade) {
+		case A:
+			printf("\n excellent");
+			break;
+		case B:
+			printf("\ngood");
+			break;
+		case C:
+			printf("\nfail");
+			break;
+		default:
+			printf("\n invalid grade");
+			break;
+	}
+	return 0;
+}
